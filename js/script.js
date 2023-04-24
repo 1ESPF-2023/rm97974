9bebaddd0fc7e1a77ab7fc3a1d4435ce9cf62987
Breqsz <rm97974@fiap.com.br>
2023-04-24 09:51:27 -0300

    Mudanças para resolução de exercício

--- js/script.js
+++ js/script.js
@@ -9,19 +9,46 @@ const botao = document.getElementById("btn-1");
 
 // recuperar uma lista de elementos com a função 
 // querySelectorAll
-const seletores = document.querySelectorAll("li");
+// const seletores = document.querySelectorAll("li");
 
-seletores.forEach((item)=>{
+// seletores.forEach((item)=>{
 
-    if (item.textContent == "Item-15") {
-        let meuItem = item;
-        console.log(`Items selecionado : ${meuItem.textContent}`);
-        meuItem.textContent = "TÁ DOMINADO!!"
-    }
-})
+//     if (item.textContent == "Item-15") {
+//         let meuItem = item;
+//         console.log(`Items selecionado : ${meuItem.textContent}`);
+//         meuItem.textContent = "TÁ DOMINADO!!"
+//     }
+// })
 
+//Para estudar//
 //setTimeOut
-//setIterval
+//setInterval
 //Random
+//Math.Ceil
 //Math.Random
-//Math.For
+//Math.Floor
+let tmp = ""
+
+function mudarCor() {
+    
+    let r = 0;
+    let g = 0;
+    let b = 0;
+
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+    
+    const cabecalho = document.querySelector(".cabecalho");
+    cabecalho.style.backgroundColor= `rgb(${r},${g},${b})`;
+    
+    tmp = setTimeout(mudarCor, 1000);
+
+}
+
+mudarCor();
+
+function mudarImagem(){
+    
+}
+
